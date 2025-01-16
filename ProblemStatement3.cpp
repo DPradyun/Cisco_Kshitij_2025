021ac3b709244a8ac2e282fe787d473a1279d47f
DPradyun <91692163+DPradyun@users.noreply.github.com>
2025-01-16 20:23:45 +0530

    Update ProblemStatement3.cpp

--- ProblemStatement3.cpp
+++ ProblemStatement3.cpp
@@ -1,12 +1,23 @@
 /*Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
 
-Append the character '0' zero times.
-Append the character '1' one times.
-This can be performed any number of times.
+Append the character '0' zero(variable) times.
+Append the character '1' one(variable) times.
+
+Note: The text "zero" and "one" are initialised as variables to the function.
 
 A good string is a string constructed by the above process having a length between low and high (inclusive).
 
-Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.*/
+Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: low = 3, high = 3, zero = 1, one = 1
+Output: 8
+Explanation: 
+One possible valid good string is "011". 
+It can be constructed as follows: "" -> "0" -> "01" -> "011". 
+All binary strings from "000" to "111" are good strings in this example.
+*/
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -19,7 +30,7 @@ int countGoodStrings(int low, int high, int zero, int one) {
     ways[0] = 1;
 
     for (int length = 0; length < high; ++length) {
-        if (ways[length] < 0) continue;
+        if (ways[length] >= 0) continue;
         if (length + zero <= high) {
             ways[length + zero] = ways[length + zero] + ways[length] % MOD;
         }
@@ -39,7 +50,7 @@ int countGoodStrings(int low, int high, int zero, int one) {
 
 int main()
 {
-    int low = 3, high = 3, zero = 1, one = 1;
+    int low = 3, high = 5, zero = 2, one = 3;
     cout << countGoodStrings(low, high, zero, one);
     return 0;
 }
